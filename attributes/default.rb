75820a8ca8335186291605b0eb64357b4fa2899a
James Hu <axsuul@gmail.com>
2012-09-24 13:35:34 -0700

    Add a default environment attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['application']["name"] = "My App"
 default['application']['deploy_path'] = "/srv/my-app"
 default['application']['deploy_user'] = "deployer"
+default['application']['environment'] = "production"
